e75883acc3dc8fbf1cfd6eb6d545a5ce75acf6a6
rruiz88 <roman.ruiz816@gmail.com>
2022-10-06 14:00:43 -0500

    Completed the Support Classes and passed TestTaskTwo

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -1,5 +1,7 @@
 package org.launchcode.techjobs.oo;
 
+import java.util.Objects;
+
 public class PositionType {
 
     private int id;
@@ -27,6 +29,11 @@ public class PositionType {
         return id;
     }
 
+    @Override
+    public String toString() {
+        return value;
+    }
+
     public String getValue() {
         return value;
     }
@@ -35,4 +42,16 @@ public class PositionType {
         this.value = value;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PositionType that = (PositionType) o;
+        return id == that.id && value.equals(that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, value);
+    }
 }
